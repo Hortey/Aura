31ab3b3cbd567873aad017292f44d5ed0978d1fa
Yarik <slavtuk.slavtuk2003@gmail.com>
2023-08-29 04:05:22 +0300

    made a slide on the website

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.new__slider').slick({
+        dots: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: false,
+        nextArrow: false,
+      });
+  });
